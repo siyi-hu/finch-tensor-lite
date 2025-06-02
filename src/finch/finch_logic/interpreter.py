ecb98273afcf6dc0aa316ecc0efe077d3903863d
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-06-02 07:42:18 +0200

    Convert `interpreter.py` to use a `match`-`case` statement.

--- src/finch/finch_logic/interpreter.py
+++ src/finch/finch_logic/interpreter.py
@@ -44,106 +44,104 @@ class FinchLogicInterpreter:
         if self.verbose:
             print(f"Evaluating: {node}")
         # Placeholder for actual logic
-        head = node.head()
-        if head == Immediate:
-            return node.val
-        if head == Deferred:
-            raise ValueError(
-                "The interpreter cannot evaluate a deferred node, a compiler might "
-                "generate code for it"
-            )
-        if head == Field:
-            raise ValueError("Fields cannot be used in expressions")
-        if head == Alias:
-            if node in self.bindings:
-                return self.bindings[node]
-            raise ValueError(f"undefined tensor alias {node.val}")
-        if head == Table:
-            if node.tns.head() != Immediate:
-                raise ValueError("The table data must be Immediate")
-            return TableValue(node.tns.val, node.idxs)
-        if head == MapJoin:
-            if node.op.head() != Immediate:
-                raise ValueError("The mapjoin operator must be Immediate")
-            op = node.op.val
-            args = list(map(self, node.args))
-            dims = {}
-            idxs = []
-            for arg in args:
-                for idx, dim in zip(arg.idxs, arg.tns.shape, strict=True):
-                    if idx in dims:
-                        if dims[idx] != dim:
-                            raise ValueError("Dimensions mismatched in map")
-                    else:
-                        idxs.append(idx)
-                        dims[idx] = dim
-            fill_val = op(*[fill_value(arg.tns) for arg in args])
-            dtype = return_type(op, *[element_type(arg.tns) for arg in args])
-            result = self.make_tensor(
-                tuple(dims[idx] for idx in idxs), fill_val, dtype=dtype
-            )
-            for crds in product(*[range(dims[idx]) for idx in idxs]):
-                idx_crds = dict(zip(idxs, crds, strict=True))
-                vals = [arg.tns[*[idx_crds[idx] for idx in arg.idxs]] for arg in args]
-                result[*crds] = op(*vals)
-            return TableValue(result, idxs)
-        if head == Aggregate:
-            if node.op.head() != Immediate:
-                raise ValueError("The aggregate operator must be Immediate")
-            if node.init.head() != Immediate:
-                raise ValueError("The aggregate initial value must be Immediate")
-            arg = self(node.arg)
-            init = node.init.val
-            op = node.op.val
-            dtype = fixpoint_type(op, init, element_type(arg.tns))
-            new_shape = [
-                dim
-                for (dim, idx) in zip(arg.tns.shape, arg.idxs, strict=True)
-                if idx not in node.idxs
-            ]
-            result = self.make_tensor(new_shape, init, dtype=dtype)
-            for crds in product(*[range(dim) for dim in arg.tns.shape]):
-                out_crds = [
-                    crd
-                    for (crd, idx) in zip(crds, arg.idxs, strict=True)
-                    if idx not in node.idxs
-                ]
-                result[*out_crds] = op(result[*out_crds], arg.tns[*crds])
-            return TableValue(result, [idx for idx in arg.idxs if idx not in node.idxs])
-        if head == Relabel:
-            arg = self(node.arg)
-            if len(arg.idxs) != len(node.idxs):
+        match node:
+            case Immediate(val):
+                return val
+            case Deferred(_):
                 raise ValueError(
-                    "The number of indices in the relabel must match: "
-                    f"{arg.idxs} vs {node.idxs}"
+                    "The interpreter cannot evaluate a deferred node, a compiler might "
+                    "generate code for it"
                 )
-            return TableValue(arg.tns, node.idxs)
-        if head == Reorder:
-            arg = self(node.arg)
-            for idx, dim in zip(arg.idxs, arg.tns.shape, strict=True):
-                if idx not in node.idxs and dim != 1:
-                    raise ValueError("Trying to drop a dimension that is not 1")
-            arg_dims = dict(zip(arg.idxs, arg.tns.shape, strict=True))
-            dims = [arg_dims.get(idx, 1) for idx in node.idxs]
-            result = self.make_tensor(dims, fill_value(arg.tns), dtype=arg.tns.dtype)
-            for crds in product(*[range(dim) for dim in dims]):
-                node_crds = dict(zip(node.idxs, crds, strict=True))
-                in_crds = [node_crds.get(idx, 0) for idx in arg.idxs]
-                result[*crds] = arg.tns[*in_crds]
-            return TableValue(result, node.idxs)
-        if head == Query:
-            rhs = self(node.rhs)
-            self.bindings[node.lhs] = rhs
-            return (rhs,)
-        if head == Plan:
-            res = ()
-            for body in node.bodies:
-                res = self(body)
-            return res
-        if head == Produces:
-            return tuple(self(arg).tns for arg in node.args)
-        if head == Subquery:
-            if node.lhs not in self.bindings:
-                self.bindings[node.lhs] = self(node.arg)
-            return self.bindings[node.lhs]
-        raise ValueError(f"Unknown expression type: {head}")
+            case Field(_):
+                raise ValueError("Fields cannot be used in expressions")
+            case Alias(_):
+                alias = self.bindings.get(node, None)
+                if alias is None:
+                    raise ValueError(f"undefined tensor alias {node}")
+                return alias
+            case Table(Immediate(val), idxs):
+                return TableValue(val, idxs)
+            case MapJoin(Immediate(op), args):
+                args = tuple(self(a) for a in args)
+                dims = {}
+                idxs = []
+                for arg in args:
+                    for idx, dim in zip(arg.idxs, arg.tns.shape, strict=True):
+                        if idx in dims:
+                            if dims[idx] != dim:
+                                raise ValueError("Dimensions mismatched in map")
+                        else:
+                            idxs.append(idx)
+                            dims[idx] = dim
+                fill_val = op(*[fill_value(arg.tns) for arg in args])
+                dtype = return_type(op, *[element_type(arg.tns) for arg in args])
+                result = self.make_tensor(
+                    tuple(dims[idx] for idx in idxs), fill_val, dtype=dtype
+                )
+                for crds in product(*[range(dims[idx]) for idx in idxs]):
+                    idx_crds = dict(zip(idxs, crds, strict=True))
+                    vals = [
+                        arg.tns[*[idx_crds[idx] for idx in arg.idxs]] for arg in args
+                    ]
+                    result[*crds] = op(*vals)
+                return TableValue(result, idxs)
+            case Aggregate(op, init, arg, idxs):
+                arg = self(arg)
+                init = init.val
+                op = op.val
+                dtype = fixpoint_type(op, init, element_type(arg.tns))
+                new_shape = tuple(
+                    dim
+                    for (dim, idx) in zip(arg.tns.shape, arg.idxs, strict=True)
+                    if idx not in node.idxs
+                )
+                result = self.make_tensor(new_shape, init, dtype=dtype)
+                for crds in product(*[range(dim) for dim in arg.tns.shape]):
+                    out_crds = [
+                        crd
+                        for (crd, idx) in zip(crds, arg.idxs, strict=True)
+                        if idx not in node.idxs
+                    ]
+                    result[*out_crds] = op(result[*out_crds], arg.tns[*crds])
+                return TableValue(
+                    result, [idx for idx in arg.idxs if idx not in node.idxs]
+                )
+            case Relabel(arg, idxs):
+                arg = self(arg)
+                if len(arg.idxs) != len(idxs):
+                    raise ValueError("The number of indices in the relabel must match")
+                return TableValue(arg.tns, idxs)
+            case Reorder(arg, idxs):
+                arg = self(arg)
+                for idx, dim in zip(arg.idxs, arg.tns.shape, strict=True):
+                    if idx not in idxs and dim != 1:
+                        raise ValueError("Trying to drop a dimension that is not 1")
+                arg_dims = dict(zip(arg.idxs, arg.tns.shape, strict=True))
+                dims = [arg_dims.get(idx, 1) for idx in idxs]
+                result = self.make_tensor(
+                    dims, fill_value(arg.tns), dtype=arg.tns.dtype
+                )
+                for crds in product(*[range(dim) for dim in dims]):
+                    node_crds = dict(zip(idxs, crds, strict=True))
+                    in_crds = [node_crds.get(idx, 0) for idx in arg.idxs]
+                    result[*crds] = arg.tns[*in_crds]
+                return TableValue(result, idxs)
+            case Query(lhs, rhs):
+                rhs = self(rhs)
+                self.bindings[lhs] = rhs
+                return (rhs,)
+            case Plan(bodies):
+                res = ()
+                for body in bodies:
+                    res = self(body)
+                return res
+            case Produces(args):
+                return tuple(self(arg).tns for arg in args)
+            case Subquery(lhs, arg):
+                res = self.bindings.get(lhs)
+                if res is None:
+                    res = self(arg)
+                    self.bindings[lhs] = res
+                return res
+            case _:
+                raise ValueError(f"Unknown expression type: {type(node)}")
