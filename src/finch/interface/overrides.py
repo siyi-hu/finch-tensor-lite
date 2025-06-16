02d76d9e7e9df521c686f58765d07f0d6becf805
yusheng036 <ays03@bu.edu>
2025-06-15 20:04:03 -0400

    Added Trigo functions, and bitwise_inverse,  fixed mateuz suggestion, changed overlay in lazy.py (#105)
    * Added all trigonometry functions
    
    * Fix implementation error
    
    * Added most trigo func except arcsin, arccos, arccosh and arctanh due to fill value error
    
    * Added bitwise_invert
    
    * Ruff Checks
    
    * Forgotten to add asinh into init.py
    
    * Forgotten sinh
    
    * Changed atan2(self, other)
    
    * Address the numpy change

--- src/finch/interface/overrides.py
+++ src/finch/interface/overrides.py
@@ -12,6 +12,7 @@ element_wise_ufunc_map = {
     np.positive: operator.pos,
     np.absolute: operator.abs,
     np.abs: operator.abs,
+    np.bitwise_invert: operator.invert,
     np.bitwise_and: operator.and_,
     np.bitwise_or: operator.or_,
     np.bitwise_xor: operator.xor,
@@ -21,6 +22,19 @@ element_wise_ufunc_map = {
     np.floor_divide: operator.floordiv,
     np.mod: operator.mod,
     np.pow: operator.pow,
+    np.sin: np.sin,
+    np.sinh: np.sinh,
+    np.cos: np.cos,
+    np.cosh: np.cosh,
+    np.tan: np.tan,
+    np.tanh: np.tanh,
+    np.asin: np.asin,
+    np.asinh: np.asinh,
+    np.acos: np.acos,
+    np.acosh: np.acosh,
+    np.atan: np.atan,
+    np.atanh: np.atanh,
+    np.atan2: np.atan2,
     # Add more ufuncs as needed
 }
 
